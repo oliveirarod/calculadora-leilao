d9c00b1a8d03654628607b230e94622ca2988ce4
oliveirarod <rodrigo.oliveira6446@gmail.com>
2025-01-16 12:58:34 -0300

    Adjusts configs related to deploy

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-KXYU3LUG.mjs";import"./chunk-NKA26QO3.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};
--- docs/server/render-utils.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{Ia as e,db as o,eb as r,fb as n}from"./chunk-NKA26QO3.mjs";import"./chunk-DCFYTP2A.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
