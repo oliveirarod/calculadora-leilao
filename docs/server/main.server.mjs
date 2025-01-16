bf44635c757d6bf1822738142458db84e9dc03b5
oliveirarod <rodrigo.oliveira6446@gmail.com>
2025-01-16 11:47:21 -0300

    Adjusts build

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-KXYU3LUG.mjs";import"./chunk-NKA26QO3.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};
---
+++ docs/server/render-utils.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{Ia as e,db as o,eb as r,fb as n}from"./chunk-NKA26QO3.mjs";import"./chunk-DCFYTP2A.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
